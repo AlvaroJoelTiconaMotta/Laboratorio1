9e3de31a3b706452c477c755b17c98bf612efb97
AlvaroTimo <aticonamo@unsa.edu.pe>
2020-09-13 00:27:26 -0500

    optimizando codigo

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int suma=0;
-    for(int x=3; x<1000; x++){
-        if((x % 3 == 0) || (x % 5 == 0)) 
+int main()
+{
+    int suma = 0;
+    for (int x = 3; x < 1000; x++)
+    {
+        if ((x % 3 == 0) || (x % 5 == 0))
             suma += x;
     }
-    cout<<suma;
+    cout << suma
 }
--- Ejercicio5.cpp
+++ Ejercicio5.cpp
@@ -1,21 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int arreglo[20],act=1,cont;
-    long acu=24;
-    arreglo[0]=1;
-    for(int i=2;i<=20;i++){
-        cont=0;
-        for(int j=0;j<act;j++)
-            if(i%arreglo[j]==0)
+int main()
+{
+    int arreglo[20], act = 1, cont;
+    long acu = 24;
+    arreglo[0] = 1;
+    for (int i = 2; i <= 20; i++)
+    {
+        cont = 0;
+        for (int j = 0; j < act; j++)
+            if (i % arreglo[j] == 0)
                 cont++;
-        if(cont==1){
-            arreglo[act]=i;
+        if (cont == 1)
+        {
+            arreglo[act] = i;
             act++;
         }
     }
-    for(int k=0;k<=act;k++)
-        acu*=arreglo[k];
-    cout<<acu;
+    for (int k = 0; k <= act; k++)
+        acu *= arreglo[k];
+    ¡ cout << "El numero mas pequeño divisble del 1 al 20 es: " << acu;
     return 0;
 }
